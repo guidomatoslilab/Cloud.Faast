c6467284eb1456346844884f1e2fcdc5d8f276c6
guidomatoslilab <guido.matos@lilab.pe>
2023-02-24 09:22:55 -0500

    Register interfaces automatically

---
+++ Cloud.Faast.Integracion/Extensions/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace Cloud.Faast.Integracion.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddServicesAsInterfaces(this IServiceCollection services, Assembly assembly, string serviceSuffix)
+        {
+            services.AddServicesAsInterfaces(new Assembly[] { assembly }, serviceSuffix);
+        }
+        public static void AddServicesAsInterfaces(this IServiceCollection services, Assembly[] assemblies, string serviceSuffix)
+        {
+            services.Scan(scan => scan
+                .FromAssemblies(assemblies)
+                .AddClasses(x => x.Where(c => c.Name.EndsWith(serviceSuffix)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+            );
+        }
+    }
+}
